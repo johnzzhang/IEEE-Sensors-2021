fdc21b8d37b674781ce366b40d06c79ec1c4fa5f
John <johnzhang15@gmail.com>
2021-06-25 03:25:44 -0400

    Init commit.

---
+++ benthowave.m
@@ -0,0 +1,148 @@
+%% import data
+clear; clc;
+
+dataDir = '20210624 sensor measurement';
+experiment = 'Benthowave';
+numRuns = 11;
+benthowaveData = cell(numRuns,1);
+
+runSkip = 134;
+runLabels = 2431:134:3771;
+
+% import all benthowave data into the cell array
+for run = 1:numRuns
+    runNumber = num2str(runLabels(run));
+    dataPath = ['data/' dataDir '/' experiment '/f' runNumber '.txt'];
+    benthowaveData{run} = importfile(dataPath);
+end
+
+% data summary:
+% proceeds by 2 mm increments
+% 1. above water
+% 2. at water line
+% 3. 2 mm down
+% 4. 4 mm down
+% ...
+% 11. 18 mm down
+
+
+%% process and plot data
+% summary:
+% fig. 1: frequency waterfall
+% fig. 2: acceleration and predicted pressure at 10 mm depth
+% fig. 3: pressure vs depth at 1 kHz
+
+close all;
+
+% frequency waterfall
+figure(1);
+hold on;
+
+for run = 1:2:numRuns
+    data = benthowaveData{run};
+
+    % convert voltage to pressure
+    p2v = 10^(-223.3/20)*1e6; % BII-7181 [V/Pa]
+    preampGain = 10^(60/20);  % BII-1092
+
+    benthowaveVoltage = 10.^(data.Ch1MagdB/20); % [V}
+    pressure = benthowaveVoltage/p2v/preampGain; % [Pa]
+
+    if run == 1
+        plot(data.FreqHz, pressure, 'k.-');
+    else
+        plot(data.FreqHz, pressure, '.-');
+    end
+end
+
+legend('above water','0 mm','4 mm', '8 mm', '12 mm', '16 mm');
+
+set(gca,'XScale','log');
+set(gca,'YScale','log');
+
+xlabel('frequency [Hz]');
+ylabel('pressure [Pa]');
+improvePlot();
+
+
+% predicted vs measured pressure at 12 mm
+figure(2);
+hold on;
+
+for run = 1
+    data = benthowaveData{run};
+
+    ldvVoltage = 10.^(data.Ch0MagdB/20); % [V]
+
+    vel2volt = 2e-3; % [mm/s/V]
+
+    velocity = vel2volt*ldvVoltage; % [m/s]
+    position = 1./(2*pi*data.FreqHz).*velocity; % [m]
+    acceleration = 2*pi*data.FreqHz.*velocity; % [m/s^2]
+
+    rho = 1000; % [kg/m^3]
+    g = 9.8; % [m/s^2]
+
+    h = 12e-3; % depth [m]
+
+    pressureTheoritical = abs(rho*position.*(g-(2*pi*data.FreqHz).^2*h));
+
+    plot(data.FreqHz, pressureTheoritical, 'r.-');
+end
+
+% plot for 12 mm depth
+for run = 8
+    data = benthowaveData{run};
+
+    % convert voltage to pressure
+    p2v = 10^(-223.3/20)*1e6; % BII-7181 [V/Pa]
+    preampGain = 10^(60/20);  % BII-1092
+
+    benthowaveVoltage = 10.^(data.Ch1MagdB/20); % [V}
+    pressure = benthowaveVoltage/p2v/preampGain; % [Pa]
+
+    plot(data.FreqHz, pressure, 'k.-');
+end
+
+set(gca,'XScale','log');
+set(gca,'YScale','log');
+
+xlabel('frequency [Hz]');
+ylabel('pressure [Pa]');
+legend('Benthowave h=12 mm','LDV h=12 mm');
+improvePlot();
+
+
+% pressure vs depth
+figure(3);
+hold on;
+
+depths = 0:2:18;
+pressures = [];
+
+for run = 2:numRuns
+    data = benthowaveData{run};
+
+    idx = find(1000 == data.FreqHz);
+
+    % convert voltage to pressure
+    p2v = 10^(-223.3/20)*1e6; % BII-7181 [V/Pa]
+    preampGain = 10^(60/20);  % BII-1092
+
+    benthowaveVoltage = 10.^(data.Ch1MagdB/20); % [V}
+    pressure = benthowaveVoltage/p2v/preampGain; % [Pa]
+    pressures = [pressures pressure(idx)];
+end
+
+% measured pressure
+plot(depths,pressures, '.-');
+
+% calculated pressure
+pressureVsDepth = abs(rho*position(idx).*(g-(2*pi*1000)^2.*(depths*1e-3)));
+plot(depths,pressureVsDepth,'r.-');
+
+legend('measured 1 kHz','theoritical 1 kHz');
+
+xlabel('depth [mm]');
+ylabel('pressure [Pa]');
+improvePlot();
---
+++ trapezoid.m
@@ -0,0 +1,165 @@
+%% import data
+clear; clc;
+
+dataDir = '20210624 sensor measurement';
+experiment = 'Trapezoid';
+numRuns = 11;
+trapezoidData = cell(numRuns,1);
+
+runSkip = 134;
+runLabels = 3905:134:5245;
+
+% import all trapezoid data into the cell array
+for run = 1:numRuns
+    runNumber = num2str(runLabels(run));
+    dataPath = ['data/' dataDir '/' experiment '/f' runNumber '.txt'];
+    trapezoidData{run} = importfile(dataPath);
+end
+
+% import benthowave data
+dataDir = '20210624 sensor measurement';
+experiment = 'Benthowave';
+numRuns = 11;
+benthowaveData = cell(numRuns,1);
+
+runSkip = 134;
+runLabels = 2431:134:3771;
+
+% import all benthowave data into the cell array
+for run = 1:numRuns
+    runNumber = num2str(runLabels(run));
+    dataPath = ['data/' dataDir '/' experiment '/f' runNumber '.txt'];
+    benthowaveData{run} = importfile(dataPath);
+end
+
+
+%% process and plot data
+% summary:
+% fig. 1: voltage in vessel
+close all;
+
+figure(1);
+hold on;
+
+frequency = trapezoidData{1}.FreqHz;
+
+%%%%%%%%%%%%%%%%%%%%%%
+% baselines
+%%%%%%%%%%%%%%%%%%%%%%
+dataAboveWater = trapezoidData{1};
+dataAtWaterLine = trapezoidData{2};
+
+% need to convert to volts
+plot(frequency, 10.^(dataAboveWater.Ch2MagdB/20), '.-');
+plot(frequency, 10.^(dataAtWaterLine.Ch2MagdB/20), '.-');
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+% 5 mm depth
+%%%%%%%%%%%%%%%%%%%%%%
+N = 3; % number of samples
+data5mm = zeros(length(dataAboveWater.Ch2MagdB), N);
+for run = 3:5
+   data5mm(:,run-2) = 10.^(trapezoidData{run}.Ch2MagdB/20); % [V]
+end
+
+% compute mean and standard deviation for each row
+data5mmStdDev = std(data5mm,[],2);
+data5mmMean = mean(data5mm,2);
+
+% plot 5 mm depth
+%plot(frequency, data5mmMean, '.-');
+errorbar(frequency,data5mmMean,data5mmStdDev);
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+% 10 mm depth
+%%%%%%%%%%%%%%%%%%%%%%
+data10mm = zeros(length(dataAboveWater.Ch2MagdB), N);
+for run = 6:8
+   data10mm(:,run-5) = 10.^(trapezoidData{run}.Ch2MagdB/20);
+end
+
+% compute mean and standard deviation for each row
+data10mmStdDev = std(data10mm,[],2);
+data10mmMean = mean(data10mm,2);
+
+% plot 10 mm depth
+%plot(frequency, data10mmMean, '.-');
+errorbar(frequency,data10mmMean,data10mmStdDev);
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+% 15 mm depth
+%%%%%%%%%%%%%%%%%%%%%%
+data15mm = zeros(length(dataAboveWater.Ch2MagdB), N);
+for run = 9:11
+   data15mm(:,run-8) = 10.^(trapezoidData{run}.Ch2MagdB/20);
+end
+
+% compute mean and standard deviation for each row
+data15mmStdDev = std(data15mm,[],2);
+data15mmMean = mean(data15mm,2);
+
+% plot 10 mm depth
+%plot(frequency, data15mmMean, '.-');
+errorbar(frequency,data15mmMean,data15mmStdDev);
+
+set(gca,'XScale','log');
+set(gca,'YScale','log');
+
+xlabel('frequency [Hz]');
+ylabel('voltage [V]');
+
+legend('above water','water line','5 mm N=3','10 mm N=3','15 mm N=3');
+
+improvePlot();
+
+
+figure(2);
+hold on;
+%%%%%%%%%%%%%%%%%%%%%%
+% 10 mm depth
+%%%%%%%%%%%%%%%%%%%%%%
+data10mm = zeros(length(dataAboveWater.Ch2MagdB), N);
+for run = 6:8
+   data10mm(:,run-5) = 10.^(trapezoidData{run}.Ch2MagdB/20);
+end
+
+% compute mean and standard deviation for each row
+data10mmStdDev = std(data10mm,[],2);
+data10mmMean = mean(data10mm,2);
+
+% plot 10 mm depth
+plot(frequency, data10mmMean, '.-');
+%errorbar(frequency,data10mmMean,data10mmStdDev);
+
+ylabel('voltage [V]');
+improvePlot();
+
+yyaxis right
+% plot for 12 mm depth
+for run = 8
+    data = benthowaveData{run};
+
+    % convert voltage to pressure
+    p2v = 10^(-223.3/20)*1e6; % BII-7181 [V/Pa]
+    preampGain = 10^(60/20);  % BII-1092
+
+    benthowaveVoltage = 10.^(data.Ch1MagdB/20); % [V}
+    pressure = benthowaveVoltage/p2v/preampGain; % [Pa]
+
+    plot(data.FreqHz, pressure, '.-');
+end
+
+set(gca,'XScale','log');
+set(gca,'YScale','log');
+
+ylabel('pressure [Pa]');
+xlabel('frequency [Hz]');
+
+improvePlot();
+
+
+% pressure to voltage transfer function
+figure(3);
