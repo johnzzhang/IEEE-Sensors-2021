36601c5910a5f4d2d7e25c86bef86567bc7d03a9
John <johnz@mit.edu>
2021-06-29 02:09:40 -0400

    Worm analytical model. The peaks are flipped! Why?

--- worm.m
+++ worm.m
@@ -1,122 +1,6 @@
-%% import data
 importdata();
 
-%% analytical circuit
-
-f = wormData{1}.FreqHz;
-
-s = tf('s');
-
-R0 = 10e6;
-
-R1 = 34e6;
-C1 = 10e-12;
-
-f1 = 1/(2*pi*R1*C1);
-
-R2 = 10e3;
-C2 = 100e-9;
-
-f2 = 1/(2*pi*R2*C2);
-
-R3 = 100e3;
-C3 = 50e-12;
-
-f3 = 1/(2*pi*R3*C3);
-
-R4 = 1e3;
-R5 = 100e3;
-
-% first stage
-Z1 = R1/(s*C1)/(R1+1/(s*C1));
-S1 = -s*Z1; % transimpedance
-
-% second stage
-Z2 = R2 + 1/(s*C2);
-Z3 = R3/(s*C3)/(R3+1/(s*C3));
-
-S2 = -Z3/Z2;
-
-% final stage
-S3 = -R5/R4;
-
-[m, p] = bode(S1*S2*S3, 2*pi*f);
-
-
-%% worm analytical
-COMSOL_freq_top_electrode_import();
-
-w = 0.5e-3;
-l = 15e-3;
-d31_prime = 22e-12;
-d32_prime = 3e-12;
-d33_prime = -30e-12;
-
-E = 5.7e9;
-% add damping by making E complex
-zeta = [0 0.1 0.5 1];
-figure;
-subplot(2,1,1);
-ylabel('Magnitude [C/Pa]')
-for index = 1:length(zeta)
-    E = E + zeta(index)*E*1i;
-    nu = 0.35;
-    rho = 1780;
-
-    c = sqrt(E/rho);
-
-    P0 = 1; % Pascals
-
-    piezo_angle = 0;
-    d31 = cos(piezo_angle).^2*d31_prime+sin(piezo_angle).^2*d32_prime;
-    d32 = sin(piezo_angle).^2*d31_prime+cos(piezo_angle).^2*d32_prime;
-    d33 = d33_prime*ones(size(piezo_angle));
-
-    freq = logspace(log10(200),6,600);
-    ang_freq = 2*pi*freq_comsol';
-    k = ang_freq/c;
-
-    F11 = -P0*w*((1-2*nu)*tan(k*l)./k+2*nu*l);
-    F22 = -P0*w*l*ones(size(ang_freq));
-    F33 = F22;
-
-    Q31 = d31*F11;
-    Q32 = d32*F22;
-    Q33 = d33*F33;
-
-    Q = Q31+Q32+Q33;
-
-    hearing_range = freq_comsol < 20e3;
-
-    %cost_func = 'NRMSE';
-    %fit = goodnessOfFit(abs((Q(hearing_range))'),abs(Q_comsol((hearing_range))),cost_func);
-    %error = (1-fit)*100;
-
-    subplot(2,1,1);
-    hold on;
-    plot(freq_comsol,abs(Q),'-');
-    xlim([200 max(freq_comsol)]);
-    set(gca, 'YScale', 'log')
-    set(gca, 'XScale', 'log')
-    
-    subplot(2,1,2);
-    hold on;
-    plot(freq_comsol,180/pi*angle(Q),'-');
-    set(gca, 'YScale', 'linear')
-    set(gca, 'XScale', 'log')
-    xlabel('Frequency [Hz]')
-    ylabel('Phase [^\circ]')
-end
-hold off;
-subplot(2,1,1);
-plot(freq_comsol,abs(Q_comsol),'k.');
-legend([ '\zeta = ' + string(zeta) 'COMSOL']);
-    
-improvePlot();
-
-close all;
-
-%% Analytical
+%% Analytical Model
 f = benthowaveData{1}.FreqHz;
 depths = 0:2:18;
 depths = 1e-3*depths;
@@ -157,7 +41,7 @@ rho = 1780;
 
 s11 = 1/E;
 s12 = -nu/E;
-s13 = s12;
+s13 = -nu/E;
 
 c = sqrt(E/rho);
 
@@ -166,11 +50,16 @@ k = (2*pi*f)/c;
 h = 10e-3;
 
 % part without pressure varying
-Q = -w*d31/s11*(s11+s12+s13).*pressureDistribution{i}(h).*tan(k*l)./k;
+Q = zeros(size(f));
+for i = 1:length(Q)
+    Q = -d31/s11*w*(s11+s12+s13).*pressureDistribution{i}(h).*tan(k*l)./k;
+end
 Qpressure = zeros(size(Q));
+Qtotal = zeros(size(Q));
 
 for i = 1:length(Q)
-    Qpressure(i) = Q(i) + w*((s12+s13)/s11*d31-d32-d33)*integral(pressureDistribution{i}, 0, 10e-3, 'ArrayValued', true);
+    Qpressure(i) = w*((s12+s13)/s11*d31-d32-d33)*integral(pressureDistribution{i}, 0, h, 'ArrayValued', true);
+    Qtotal(i) = Q(i) + Qpressure(i);
 end
 
 % analytical circuit
@@ -213,21 +102,12 @@ S3 = -R5/R4;
 
 %% process and plot
 
-% summary:
-% fig. 1: voltage in vessel
-% close all;
-% 
-figure(1);
-hold on;
-
 frequency = wormData{1}.FreqHz;
 
 %%%%%%%%%%%%%%%%%%%%%%
 % baselines
 %%%%%%%%%%%%%%%%%%%%%%
 dataAtWaterLine = wormData{1};
-
-%plot(frequency, 10.^(dataAtWaterLine.Ch2MagdB/20), '.-');
 
 %%%%%%%%%%%%%%%%%%%%%%
 % 10 mm depth
@@ -245,16 +125,6 @@ data10mmStdDev = std(data10mm,[],2);
 data10mmMean = mean(data10mm,2);
 data10mmPhaseDegMean = mean(data10mmPhaseDeg,2);
 
-% plot 10 mm depth
-%plot(frequency, data10mmMean, '.-');
-errorbar(frequency,data10mmMean,data10mmStdDev);
-
-set(gca,'XScale','log');
-set(gca,'YScale','log');
-
-xlim([100 10000]);
-
-yyaxis right
 
 for run = 7
     data = benthowaveData{run};
@@ -266,48 +136,44 @@ for run = 7
     benthowaveVoltage = 10.^(data.Ch1MagdB/20); % [V}
     pressure = benthowaveVoltage/p2v/preampGain; % [Pa]
     pressurePhaseDeg = 360*data.Ch1Phasecyc;
-
-    plot(data.FreqHz, pressure, '.-');
 end
-
-set(gca,'XScale','log');
-set(gca,'YScale','log');
-
-ylabel('pressure [Pa]');
-xlabel('frequency [Hz]');
-xlim([100 10000]);
-
-improvePlot();
-close all;
 
 %% pressure to voltage transfer function
 figure(2);
 
 xlims = [200 20e3];
 
-subplot(211);
+%subplot(211);
 hold on;
 plot(f, data10mmMean./pressure, 'r.-');
+%errorbar(frequency,data10mmMean./pressure,data10mmStdDev./pressure);
 
 % plot voltage from analytical
-plot(f, m(:).*abs(Qpressure), 'k.-');
+%plot(f, m(:).*abs(Qpressure), 'm.-');
+%plot(f, m(:).*abs(Q), 'g.-');
+plot(f, m(:).*abs(Qtotal), 'k.-');
+
+legend('measured','theory');
 
 xlim(xlims);
 
 set(gca,'XScale','log');
 set(gca,'YScale','log');
 
-ylabel('mag [V/Pa]');
+ylabel('charge amp output [V]');
 xlabel('frequency [Hz]');
 
-subplot(212);
-wormPhase = data10mmPhaseDegMean - pressurePhaseDeg;
-wormPhase(2) = wormPhase(1) + 360; % manual unwrap
-plot(frequency, unwrap(wormPhase), 'r.-');
+% plotting phase doesn't make sense for frequency response
+% subplot(212);
+% hold on;
+% wormPhase = data10mmPhaseDegMean - pressurePhaseDeg;
+% wormPhase(2) = wormPhase(1) + 360; % manual unwrap
+% plot(f, unwrap(wormPhase)+360, 'r.-');
+% plot(f, p(:), 'k.-');
 
 xlim(xlims);
 set(gca,'XScale','log');
-ylabel('phase [deg]');
+%ylabel('phase [deg]');
 xlabel('frequency [Hz]');
 
 improvePlot();
